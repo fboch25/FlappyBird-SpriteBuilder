8b51b134c3d0e6ff45732a12d6723c24e844d7ee
fboch25 <fboch25@aol.com>
2014-12-06 00:06:04 -0500

    Automatically Committed for MakeGamesWithUs

--- FlappyBird.spritebuilder/Source/GamePlayScene.m
+++ FlappyBird.spritebuilder/Source/GamePlayScene.m
@@ -17,14 +17,23 @@
 -(void)update:(CCTime)delta
 {
     // put update code here
+    timeSinceObstacle += delta;
+    if (timeSinceObstacle > 2.0f)
     
-    
-}
+    {
 
+[self addObstacle];
+
+timeSinceObstacle = 0.0f;
+
+
+}
 // put new methods here
 -(void)touchBegan:(UITouch *)touch withEvent:(UIEvent *)event {
     [character flap];
     [self addObstacle];
+    timeSinceObstacle = 0.0f;
+    
     
     
 }
